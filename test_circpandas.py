307f6a3249e6a9c235d706869d2db7e7898d0d27
Taiki Kanda <taiki@konnyak.com>
2021-12-31 14:14:22 +0900

    add test cases.

--- test_circpandas.py
+++ test_circpandas.py
@@ -4,8 +4,11 @@ from numpy import testing as npt
 import pandas as pd
 
 def test_mean():
-    s = pd.Series([160.0, -170.0])
+    ss = [
+        pd.Series([160.0, -170.0]),
+        pd.Series([45.0, -45.0, 0.0]),
+    ]
 
-    desired = 175.0
-    actual = s.circ.mean(radian=False)
+    desired = [175.0, 0.0]
+    actual = [s.circ.mean(radian=False) for s in ss]
     npt.assert_allclose(actual, desired)
